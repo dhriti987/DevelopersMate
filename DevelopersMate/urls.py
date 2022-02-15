9131d87c2010d1b6ca20c5d39bb69728c0b5e63b
Dhritiman Bharadwaj <dhritimanbhardwaj@gmail.com>
2022-02-15 12:43:27 +0530

    JWT Token Authentication Implemented

--- DevelopersMate/urls.py
+++ DevelopersMate/urls.py
@@ -17,11 +17,17 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/',include('authentication.urls')),
-    path('profile/',include('userprofile.urls'))
+    path('profile/',include('userprofile.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 urlpatterns+=static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,5 +1,8 @@
 from rest_framework.response import Response
 from rest_framework import generics,status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from userprofile.models import Profile, Skill
 
 from userprofile.serializers import ProfileSerializer, SkillSerializer
@@ -7,14 +10,17 @@ from userprofile.serializers import ProfileSerializer, SkillSerializer
 # Create your views here.
 
 class ProfileView(generics.GenericAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
     serializer_class = ProfileSerializer
     
     def get_queryset(self):
-        return Profile.objects.all()
+        return Profile.objects.get(user = self.request.user)
 
     def get(self,request):
         query = self.get_queryset()
-        serialized_data = self.serializer_class(query,many=True)
+        serialized_data = self.serializer_class(query)
         return Response(serialized_data.data,status=status.HTTP_200_OK)
 
     def post(self,request):
